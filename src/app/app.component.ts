cc4294a2be9a4137eb39a77256514f42a2c6f786
Spencer Hewitt <kiestopoplar@gmail.com>
2021-06-09 20:44:36 -0400

    script to ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'valorant-chatcolor';
+  constructor() {
+    this.loadScripts();
+  }
+  loadScripts() {
+    let colors = [
+      '<warning>',
+      '<enemy>',
+      '<team>',
+      '<notification>',
+      '<system>',
+    ]
+    
+    
+    function rainbow(){
+      var colorThis = (<HTMLInputElement>document.getElementById('input')).value;
+      var j = 0;
+      var ret = "";
+      for(var i = 0; i < colorThis.length; i++){
+          if(colorThis.charAt(i) != ' '){
+              if(j < 5) {// colors
+                  ret = ret.concat(colors[j]);
+                  ret = ret.concat(colorThis.charAt(i));
+                  ret = ret.concat("</>");
+                  j++;
+              }else{// white
+                  ret = ret.concat(colorThis.charAt(i));
+                  j = 0;
+              }
+          }else{// disregard white space
+              ret = ret.concat(' ');
+          }
+      }
+      console.log(ret);
+     (<HTMLInputElement>document.getElementById("output")).innerText = ret;
+    }
+    
+  }
 }
+
